4d873c29499e30a25a4139753958ddbf16ab54d8
Ahmad Raza <mustafaraza03898@gmail.com>
2025-06-06 08:48:24 +0530

    adding Candidate  entity

--- src/main/java/com/ahmad/projects/voting_application/entities/ElectionResult.java
+++ src/main/java/com/ahmad/projects/voting_application/entities/ElectionResult.java
@@ -1,4 +1,23 @@
 package com.ahmad.projects.voting_application.entities;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Entity
+@Data
 public class ElectionResult {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long  electionId;
+
+    @NotBlank(message = "Election name is required.")
+    private String electionName;
+
+    @OneToOne
+    @JoinColumn(name = "winner_id")
+    private Candidate winner;
+
+    private Integer totalVote;
 }
