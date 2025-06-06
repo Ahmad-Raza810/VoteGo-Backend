60630e0b9f8bc01c6bf0eed762378e3de9be60b6
Ahmad Raza <mustafaraza03898@gmail.com>
2025-06-06 09:48:09 +0530

    Adding Vote Repo.

--- src/main/java/com/ahmad/projects/voting_application/repository/VoteRepository.java
+++ src/main/java/com/ahmad/projects/voting_application/repository/VoteRepository.java
@@ -1,4 +1,12 @@
 package com.ahmad.projects.voting_application.repository;
 
-public interface VoteRepository {
+import com.ahmad.projects.voting_application.entities.Vote;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.nio.file.LinkOption;
+
+@Repository
+public interface VoteRepository extends JpaRepository<Vote,Long> {
+
 }
