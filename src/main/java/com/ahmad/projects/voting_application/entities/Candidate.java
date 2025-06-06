d5e064a2d99f62de3b537cd654da3ed6781f14da
Ahmad Raza <mustafaraza03898@gmail.com>
2025-06-06 08:48:03 +0530

    adding Candidate  entity

--- src/main/java/com/ahmad/projects/voting_application/entities/Candidate.java
+++ src/main/java/com/ahmad/projects/voting_application/entities/Candidate.java
@@ -1,4 +1,28 @@
 package com.ahmad.projects.voting_application.entities;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+import java.util.List;
+
+@Entity
+@Data
 public class Candidate {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long candidateId;
+
+    @NotBlank(message = "Name is required.")
+    private Long name;
+
+    @NotBlank(message = "Party name is required.")
+    private String party;
+
+    private Integer voteCount=0;
+
+    @OneToMany(mappedBy="candidate",cascade = CascadeType.ALL)
+    private List<Vote> vote;
+
 }
