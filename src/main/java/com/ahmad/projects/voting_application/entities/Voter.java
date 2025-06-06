5b54191fd7d920d09555fa750eafd5cf0d3423b2
Ahmad Raza <mustafaraza03898@gmail.com>
2025-06-06 08:47:16 +0530

    adding Voter Entity

--- src/main/java/com/ahmad/projects/voting_application/entities/Voter.java
+++ src/main/java/com/ahmad/projects/voting_application/entities/Voter.java
@@ -1,4 +1,28 @@
 package com.ahmad.projects.voting_application.entities;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Entity
+@Data
 public class Voter {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long voterId;
+
+    @NotBlank(message="Name is required.")
+    private String name;
+
+    private boolean hasVoted=false;
+
+    @NotBlank(message = "Email is required.")
+    @Email(message = "Email should be in valid format.")
+    private String email;
+
+    @OneToOne(mappedBy = "voter",cascade = CascadeType.ALL)
+    private Vote vote;
+
 }
