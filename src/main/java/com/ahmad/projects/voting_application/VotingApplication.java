8b6fd436519363830595d33caa6cef935f0038a0
Ahmad Raza <mustafaraza03898@gmail.com>
2025-06-06 07:18:24 +0530

    intial commit

---
+++ src/main/java/com/ahmad/projects/voting_application/VotingApplication.java
@@ -0,0 +1,13 @@
+package com.ahmad.projects.voting_application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VotingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ahmad/projects/voting_application/VotingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ahmad.projects.voting_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
