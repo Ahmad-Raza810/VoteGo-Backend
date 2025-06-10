d35d0aa4826045792fc3b6bd1d12bf166cbee4d5
Ahmad Raza <mustafaraza03898@gmail.com>
2025-06-10 09:43:29 +0530

    Module one completed.

--- src/main/java/com/ahmad/projects/votego/repository/VoterRepository.java
+++ src/main/java/com/ahmad/projects/votego/repository/VoterRepository.java
@@ -11,7 +11,7 @@ import java.util.Optional;
 @Repository
 public interface VoterRepository extends JpaRepository<Voter,Long> {
 
-    boolean  existByEmail(String Email);
+    boolean  existsByEmail(String Email);
 
     Optional<List<Voter>> findByName(String name);
 
