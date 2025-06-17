9214f176427b863bb478e756ab93beb7c730f011
Ahmad Raza <mustafaraza03898@gmail.com>
2025-06-17 07:37:16 +0530

    Service for vote created.

--- src/main/java/com/ahmad/projects/votego/service/VoteServiceImpl.java
+++ src/main/java/com/ahmad/projects/votego/service/VoteServiceImpl.java
@@ -1,4 +1,15 @@
 package com.ahmad.projects.votego.service;
 
-public class VoteServiceImpl {
+import com.ahmad.projects.votego.entities.Vote;
+
+public class VoteServiceImpl implements VoteService{
+    @Override
+    public Vote addVote(Vote vote) {
+        return null;
+    }
+
+    @Override
+    public void deleteVote(Long voteId) {
+
+    }
 }
