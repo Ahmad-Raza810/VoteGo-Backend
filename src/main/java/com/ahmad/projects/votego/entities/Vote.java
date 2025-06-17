0f1207fbfea533801ed351b7917326478a4bbbef
Ahmad Raza <mustafaraza03898@gmail.com>
2025-06-17 07:34:31 +0530

    applying filtering in vote.java

--- src/main/java/com/ahmad/projects/votego/entities/Vote.java
+++ src/main/java/com/ahmad/projects/votego/entities/Vote.java
@@ -1,6 +1,8 @@
 package com.ahmad.projects.votego.entities;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.Data;
 
@@ -14,9 +16,21 @@ public class Vote {
 
     @OneToOne
     @JoinColumn(name = "voter_id", unique = true)
+    @JsonIgnore
     private Voter voter;
 
     @ManyToOne
     @JoinColumn(name = "candidate_id")
+    @JsonIgnore
     private Candidate candidate;
+
+    @JsonProperty("voterId")
+    public Long getVoterId(){
+        return voter!=null?voter.getVoterId():null;
+    }
+
+    @JsonProperty("candidateId")
+    public Long getCandidateId(){
+        return candidate!=null?candidate.getCandidateId():null;
+    }
 }
