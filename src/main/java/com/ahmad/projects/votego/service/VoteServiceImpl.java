1db193e6a6d542496857cacf3fc22318099671e2
Ahmad Raza <mustafaraza03898@gmail.com>
2025-06-17 09:38:44 +0530

    implementing Cast vote mechanism in vote service .

--- src/main/java/com/ahmad/projects/votego/service/VoteServiceImpl.java
+++ src/main/java/com/ahmad/projects/votego/service/VoteServiceImpl.java
@@ -1,15 +1,59 @@
 package com.ahmad.projects.votego.service;
 
 import com.ahmad.projects.votego.entities.Vote;
+import com.ahmad.projects.votego.entities.Candidate;
+import com.ahmad.projects.votego.entities.Voter;
+import com.ahmad.projects.votego.exception.ResourceNotFoundException;
+import com.ahmad.projects.votego.exception.VoteNotAllowedException;
+import com.ahmad.projects.votego.repository.CandidateRepository;
+import com.ahmad.projects.votego.repository.VoteRepository;
+import com.ahmad.projects.votego.repository.VoterRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Service
 public class VoteServiceImpl implements VoteService{
+    @Autowired
+    private VoteRepository voteRepository;
+    @Autowired
+    private VoterRepository voterRepository;
+    @Autowired
+    private CandidateRepository candidateRepository;
+
+    public VoteServiceImpl(VoterRepository voterRepository, VoteRepository voteRepository, CandidateRepository candidateRepository) {
+        this.voterRepository = voterRepository;
+        this.voteRepository = voteRepository;
+        this.candidateRepository = candidateRepository;
+    }
+
     @Override
-    public Vote addVote(Vote vote) {
+    public List<Vote> getAllVote() {
+        return List.of();
+    }
+
+    @Override
+    @Transactional
+    public Vote voteCast(Long voterId, Long candidateId) {
+        Voter voter=voterRepository.findById(voterId).orElseThrow(()-> new ResourceNotFoundException("Voter with id '"+voterId+"' not exists."));
+        Candidate candidate=candidateRepository.findById(candidateId).orElseThrow(()-> new ResourceNotFoundException("Candidate with id '"+candidateId+"' not exists."));
+
+        if (voter.isHasVoted()) {
+            throw new VoteNotAllowedException("Voter with id "+voterId+" has already vote casted.");
+        }
+        Vote vote=new Vote();
+        vote.setVoter(voter);
+        vote.setCandidate(candidate);
+        voteRepository.save(vote);
+
+        candidate.setVoteCount(candidate.getVoteCount()+1);
+        candidateRepository.save(candidate);
+
+        voter.setHasVoted(true);
+        voterRepository.save(voter);
         return null;
     }
 
-    @Override
-    public void deleteVote(Long voteId) {
-
-    }
 }
